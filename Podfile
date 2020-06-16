378650b67277a3549d4392d12429903c94b98f9c
Martin <becauseyolead@gmail.com>
2020-06-16 14:54:52 +0800

    Finish time function (first part)

--- Podfile
+++ Podfile
@@ -14,5 +14,6 @@ target 'Timely' do
 
   pod 'SDWebImage'
   pod 'LLCycleScrollView'
+  pod 'SnapKit'
 
 end
---
+++ Timely/Models/Timer/CommonFunction.swift
@@ -0,0 +1,36 @@
+//
+//  CommonFunction.swift
+//  Timely
+//
+//  Created by Mac on 2020/6/12.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+import UIKit
+
+class CommonFunction: NSObject {
+
+    static public func getCalendar() -> Calendar {
+        // *** create calendar object ***
+        var calendar = Calendar.current
+        // *** define calendar components to use as well Timezone to UTC ***
+        calendar.timeZone = TimeZone(identifier: "Asia/Taipei")!
+        
+        return calendar
+    }
+    
+    static public func getHour() -> String {
+        let number = getCalendar().component(.hour, from: Date())
+        return String(format: "%02d", number)
+    }
+    static public func getMinute() -> String {
+        let number = getCalendar().component(.minute, from: Date())
+        return String(format: "%02d", number)
+    }
+    static public func getSecond() -> String {
+        let number = getCalendar().component(.second, from: Date())
+        return String(format: "%02d", number)
+    }
+}
+
+
